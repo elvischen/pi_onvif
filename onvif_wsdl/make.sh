d81a0a49c2ea62e9f590356529c1360d39b73e37
hugh <522146829@qq.com>
2017-07-09 14:49:39 +0000

    添加onvif_wsdl

---
+++ onvif_wsdl/make.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# gsoap版本: 2.8.48
+
+mkdir all
+strall="devicemgmt.wsdl"
+echo $strall
+echo start wsdl2h ...
+./wsdl2h -sck  -o all/all.h -Nabc -t./typemap.dat $strall
+cp ./onvif-header.h all/
+cd all
+cat all.h >> ./onvif-header.h
+mv onvif-header.h all.h
+cd ..
+./soapcpp2 -c -L -w -x  all/all.h -Iimport -Icustom -dall -n -ponvif
